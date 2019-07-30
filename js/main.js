6f3d8feead7018ca2aa541a723154a3542044d64
somang <somang.nam@gmail.com>
2019-07-30 18:51:01 -0400

    update

--- js/main.js
+++ js/main.js
@@ -1,9 +1,21 @@
 $( document ).ready(function() {
+    //$("#about").css('color', "#FF4500");
+
+
     // Add actionlistners for each navigation items
+    $("#home").click(function(){
+        console.log("home");
+        $("#about_page").show();
+        $("#cv_page").hide();
+        $("#portfolio_page").hide();
+        $("#contact_page").hide();        
+    });
+
     $("#about").click(function(){
         console.log("about");
         $("#about_page").show();
         $("#cv_page").hide();
+        $("#portfolio_page").hide();
         $("#contact_page").hide();        
     });
 
@@ -11,6 +23,15 @@ $( document ).ready(function() {
         console.log("cv");
         $("#about_page").hide();
         $("#cv_page").show();
+        $("#portfolio_page").hide();
+        $("#contact_page").hide();
+    });
+
+    $("#portfolio").click(function(){
+        console.log("portfolio");
+        $("#about_page").hide();
+        $("#cv_page").hide();
+        $("#portfolio_page").show();
         $("#contact_page").hide();
     });
 
@@ -18,6 +39,11 @@ $( document ).ready(function() {
         console.log("contact");
         $("#about_page").hide();
         $("#cv_page").hide();
+        $("#portfolio_page").hide();
         $("#contact_page").show();
+    });
+
+    $("#js-navbar-toggle").click(function(){
+        $(".main-nav").toggleClass('active');
     });
 });
